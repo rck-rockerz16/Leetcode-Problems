fd62cb9cd056d8cefa8d1c04503d7179e152a930
Rahul Gorai <117095417+rck-rockerz16@users.noreply.github.com>
2024-06-17 09:19:44 +0530

    Time: 13 ms (92.54%), Space: 27.4 MB (64.76%) - LeetHub

--- 1839-decode-xored-array/1839-decode-xored-array.cpp
+++ 1839-decode-xored-array/1839-decode-xored-array.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
+        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+        
         int sizeOfEncoded = encoded.size();
         vector<int> arr(sizeOfEncoded + 1);
-        
+
         arr[0] = first;
         for (int i = 0; i < sizeOfEncoded; i++)
             arr[i + 1] = encoded[i] ^ arr[i];
