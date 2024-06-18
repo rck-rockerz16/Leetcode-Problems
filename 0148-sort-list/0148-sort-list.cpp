89c9c21fcce3e9c1d0eaebc258f3e4c3e60e0b4e
Rahul Gorai <117095417+rck-rockerz16@users.noreply.github.com>
2024-06-18 09:20:43 +0530

    Time: 149 ms (20.59%), Space: 73.5 MB (37.62%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* getMidNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge(ListNode* left, ListNode* right) {
+        if (!left)
+            return right;
+        if (!right)
+            return left;
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* ptr = dummy;
+        while (left && right) {
+            if (left->val < right->val) {
+                ptr->next = left;
+                ptr = left;
+                left = left->next;
+            } else {
+                ptr->next = right;
+                ptr = right;
+                right = right->next;
+            }
+        }
+        while (left) {
+            ptr->next = left;
+            ptr = left;
+            left = left->next;
+        }
+        while (right) {
+            ptr->next = right;
+            ptr = right;
+            right = right->next;
+        }
+        return dummy->next;
+    }
+
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+
+        ListNode* midNode = getMidNode(head);
+
+        ListNode* left = head;
+        ListNode* right = midNode->next;
+        midNode->next = nullptr;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        ListNode* sortedList = merge(left, right);
+        return sortedList;
+    }
+};
