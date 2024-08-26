7595c77e01ebeca69c54f7e4347da787b2286bfc
Rahul Gorai <117095417+rck-rockerz16@users.noreply.github.com>
2024-08-26 10:35:15 +0530

    Time: 5 ms (18.52%), Space: 10.3 MB (23.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int size = nums.size();
-        int red = 0, white = 0;
-        for (int i : nums) {
-            if (!i)
-                red++;
-            else if (i == 1)
-                white++;
-        }
-        for (int itr = 0; itr < size; itr++) {
-            if (itr < red)
-                nums[itr] = 0;
-            else if (itr >= red && itr < (red + white))
-                nums[itr] = 1;
-            else
-                nums[itr] = 2;
+        int zeroBoundary = 0, current = 0, twoBoundary = nums.size() - 1;
+        while (current <= twoBoundary) {
+            if (nums[current] == 0) {
+                swap(nums[zeroBoundary], nums[current]);
+                zeroBoundary++, current++;
+            } else if (nums[current] == 1) {
+                current++;
+            } else {
+                swap(nums[current], nums[twoBoundary]);
+                twoBoundary--;
+            }
         }
     }
 };
+/*
+int size = nums.size();
+int red = 0, white = 0;
+for (int i : nums) {
+    if (!i)
+        red++;
+    else if (i == 1)
+        white++;
+}
+for (int itr = 0; itr < size; itr++) {
+    if (itr < red)
+        nums[itr] = 0;
+    else if (itr >= red && itr < (red + white))
+        nums[itr] = 1;
+    else
+        nums[itr] = 2;
+}
+*/
